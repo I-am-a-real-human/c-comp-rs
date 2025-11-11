6a087b011a0a33facb852c8206cbfb6788bf66be
I-am-a-real-human <finn.gurry@gmail.com>
2025-11-12 07:54:13 +1100

    Created compiler driver shell script. Simply just script spec

---
+++ fcc.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+# get first argument
+
+
+# validate file exists
+
+# extract path of source
+
+# check for arguments
+# valid arguments include:
+#  --lex       runs lexer and stops
+#  --parse     runs lexer and parser and stops
+#  -h | --help display a help message
+#  --codegen   runs lexer and parser and assembly generation, but stops before code emission
+#  -S          spit out assembly file 
+
+
+
