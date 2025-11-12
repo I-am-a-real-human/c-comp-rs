205a09506a1699430210e87ae8147220edea266b
I-am-a-real-human <finn.gurry@gmail.com>
2025-11-12 20:17:25 +1100

    Initial Lexer and Token structs and enums. Default struct impl for Lexer needs to be reviewed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,71 @@
+#[derive(Debug)]
+enum TokenType {
+    KEYWORD(String),
+    CONSTANT(String),
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+    SEMICOLON,
+    RETURN,
+    EOF,
+}
+
+#[derive(Debug)]
+struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    literal: String,
+    line: usize,
+}
+
+#[derive(Debug)]
+struct Lexer {
+    source: String,
+    tokens: Vec<TokenType>,
+    start: usize,
+    curr_pos: usize,
+    line: usize,
+}
+
+impl Default for Lexer {
+    fn default() -> Self {
+        Lexer {
+            source: "".to_string(),
+            tokens: vec![],
+            start: 0,
+            curr_pos: 0,
+            line: 1,
+        }
+    }
+}
+
+impl Lexer {
+    fn scan_token(&self) {}
+
+    fn tokenise(&mut self) {
+        while !self.eof() {
+            self.start = self.curr_pos;
+            self.scan_token();
+        }
+
+        self.tokens.push(Token {
+            token_type: TokenType::EOF,
+            lexeme: "",
+            literal: "",
+            line: self.line,
+        });
+    }
+
+    fn eof(&self) -> bool {
+        self.curr_pos >= self.source.len()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let program = "int main(void) {
+        return 0;
+    }"
+    .to_string();
+    let lexer = Lexer { source: program };
 }
