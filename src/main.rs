94e2a4d7f7a14f946f81b78960891c0817a2afd5
I-am-a-real-human <finn.gurry@gmail.com>
2025-11-20 08:46:22 +1100

    Additional parsing methods
    Added a host of lexing methods to assist in more complicated tokenising.

--- src/main.rs
+++ src/main.rs
@@ -79,6 +79,78 @@ impl Lexer {
         return self.at(self.curr_pos);
     }
 
+    fn conditional_token(
+        &mut self,
+        token: char,
+        match_token: TokenType,
+        original_token: TokenType,
+    ) {
+        if self.matches(token) {
+            self.add_token(match_token, "".to_string())
+        } else {
+            self.add_token(original_token, "".to_string())
+        }
+    }
+
+    fn peek(&self) -> char {
+        if self.eof() {
+            return '\0';
+        } else {
+            return self.at(self.curr_pos);
+        }
+    }
+
+    fn parse_slash(&mut self) {
+        // single line comment
+        if self.matches('/') {
+            // consume until end of line or EOF
+            while self.peek() != '\n' && !self.eof() {
+                self.advance();
+            }
+        } else {
+            self.add_token(TokenType::SLASH, "".to_string());
+        }
+    }
+
+    fn consume_string(&mut self) {
+        while self.peek() != '"' && !self.eof() {
+            if self.peek() == '\n' {
+                self.line += 1;
+            }
+            self.advance();
+        }
+
+        if self.eof() {
+            panic!("Unterminated string at line {}", self.line);
+        }
+
+        // need to call this to consume the closing '"'
+
+        self.advance();
+
+        let constant: String = self.source[self.start + 1..self.curr_pos - 1].to_string();
+        self.add_token(TokenType::CONSTANT(constant.clone()), constant);
+    }
+
+    fn consume_char(&mut self) {
+        // peek(2) == '\''? if not then report unterminated/invalid char
+        todo!();
+    }
+
+    fn consume_number(&mut self) {
+        // similar to string implementation
+        todo!();
+    }
+
+    fn is_digit(&self, c: char) -> bool {
+        // TODO move this and other generic functions outside of impl scope
+        // rely on ASCII value comparison
+        if c >= '0' && c <= '9' {
+            return true;
+        }
+        false
+    }
+
     fn scan_token(&mut self) {
         let c = self.advance();
         match c {
