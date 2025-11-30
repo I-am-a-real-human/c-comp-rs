45cce6df20f4071e724251b5e584d44e91c5ff54
I-am-a-real-human <finn.gurry@gmail.com>
2025-11-30 14:21:31 +1100

    chore: update main for testing
    Updating main function for simple testing of Parser

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,17 @@ mod parser;
 use crate::lexer::Lexer;
 use crate::parser::Parser;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = std::env::args().collect();
 
-    if args.len() <= 1 {
-        println!("fcc help TODO");
-        return ();
-    }
-
-    let source = fs::read_to_string(&args[1]).expect("Couldn't read file '{}'");
+    // let source = fs::read_to_string(&args[1]).expect("Couldn't read file '{}'");
+    let source = "int main(void){\n\treturn 0;\n}";
     let mut lexer = Lexer::from_string(&source);
 
     let result = lexer.tokenise().unwrap();
 
     let mut parser = Parser::new(result);
-    parser.parse();
+    let ast = parser.parse()?;
+    println!("{}", ast.print_tree());
+    Ok(())
 }
